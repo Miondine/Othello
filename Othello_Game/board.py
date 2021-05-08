6778674baaa24de1ba860deea2874e18fbf3544c
Paula <paula.b.hofm@gmail.com>
2021-05-08 18:29:24 +0200

    changed to use constants module

--- Othello_Game/board.py
+++ Othello_Game/board.py
@@ -4,20 +4,21 @@ Class to represent the game board. The positions on the board are saved as a two
 represents the row of the board and the second dimension the column. Each position is eihter unoccupied (0), white (1) or black (-1).
 
 '''
+import othello_game.constants as c
+
 class Board:
 
     def __init__(self):
         self.positions = []
-        self.NUM_ROWS = 8
-        self.NUM_COLS = 8
+        
         self.create_board()
 
     def create_board(self):
 
         # initialise board with unoccupied positions
-        for row in range(self.NUM_ROWS):
+        for row in range(c.NUM_ROWS):
             self.positions.append([])
-            for col in range(self.NUM_COLS):
+            for col in range(c.NUM_COLS):
                 self.positions[row].append(0)
 
         # initialise starting postition
