fdc18c1ad611314ce222f989a23bb38b58144e2b
Paula <paula.b.hofm@gmail.com>
2021-05-11 13:57:05 +0200

    changed mistakes with mixing up of row and col

--- Othello_Game/interaction.py
+++ Othello_Game/interaction.py
@@ -2,11 +2,13 @@ import pygame
 import othello_game.constants as c
 
 
-
 class Interaction:
 
     def __init__(self):
         self.window = pygame.display.set_mode((c.WIDTH,c.HEIGHT))
+        self.FPS = 60
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Othello')
 
     #draw board and 
     def draw_board(self,board):
@@ -20,20 +22,36 @@ class Interaction:
             #draw current pieces
             for col in range(c.NUM_COLS):
                 if(board.positions[row][col] == 1):
-                    pygame.draw.circle(self.window, c.WHITE, (c.MARGIN + (row + 0.5) * c.SQUARE_SIZE, c.MARGIN + (col + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)   
+                    pygame.draw.circle(self.window, c.WHITE, (c.MARGIN + (col + 0.5) * c.SQUARE_SIZE, c.MARGIN + (row + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)   
                 elif(board.positions[row][col] == -1):         
-                    pygame.draw.circle(self.window, c.BLACK, (c.MARGIN + (row + 0.5) * c.SQUARE_SIZE, c.MARGIN + (col + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
-        
+                    pygame.draw.circle(self.window, c.BLACK, (c.MARGIN + (col + 0.5) * c.SQUARE_SIZE, c.MARGIN + (row + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
+        pygame.display.update()
 
-    def draw_possible_moves(self,possible_moves,colour):
+    def draw_possible_positions(self,possible_positions,colour):
 
         if(colour == 1):
-            for move in possible_moves:
-                pygame.draw.circle(self.window, c.YELLOW, (c.MARGIN + (move[0] + 0.5) * c.SQUARE_SIZE, c.MARGIN + (move[1] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
+            for position in possible_positions:
+                pygame.draw.circle(self.window, c.YELLOW, (c.MARGIN + (position[1] + 0.5) * c.SQUARE_SIZE, c.MARGIN + (position[0] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
         if(colour == -1):
-            for move in possible_moves:
-                pygame.draw.circle(self.window, c.ORANGE, (c.MARGIN + (move[0] + 0.5) * c.SQUARE_SIZE, c.MARGIN + (move[1] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
+            for position in possible_positions:
+                pygame.draw.circle(self.window, c.ORANGE, (c.MARGIN + (position[1] + 0.5) * c.SQUARE_SIZE, c.MARGIN + (position[0] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
+        pygame.display.update()
 
     def draw_selected_position(self,position):
 
-        pygame.draw.circle(self.window, c.DARK_RED, (c.MARGIN + (position[0] + 0.5) * c.SQUARE_SIZE, c.MARGIN + (position[1] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
+        pygame.draw.circle(self.window, c.DARK_RED, (c.MARGIN + (position[1] + 0.5) * c.SQUARE_SIZE, c.MARGIN + (position[0] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
+        pygame.display.update()
+
+    def get_mouse_position(self):
+
+        while True:
+            self.clock.tick(self.FPS)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return [100,0]
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x,y = pygame.mouse.get_pos()
+                    row = int((y - c.MARGIN) / c.SQUARE_SIZE)
+                    col = int((x - c.MARGIN) / c.SQUARE_SIZE)
+                    return [row,col]
+
