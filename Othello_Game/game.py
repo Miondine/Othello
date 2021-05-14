67a2569d5de118a52cb694c75e6bc0ccfe18831d
Paula <paula.b.hofm@gmail.com>
2021-05-14 14:04:25 +0200

    added comments

--- Othello_Game/game.py
+++ Othello_Game/game.py
@@ -1,28 +1,31 @@
+'''
+Game object represents one game played between two players. Handles main loop over the game, where each player makes a move or passes each round. 
+Can either handle a game without graphical output or with. Sets winner and number of disks in the end.
+'''
+
 import othello_game.board as board
 import othello_game.interaction as interaction
 import othello_player.player as player
 import othello_player.human as human
 
-'''
-input: 
-typ_player1/2: (String) Typ of player 1/2 one of the following: (HUMAN,ROXANNE)
-name_player1/2: (String) name of player1/2
-graphical: (Boolean) If true then game will be with graphical output, if false no graphical output.
-            If one typ of player is HUMAN then there will be graphical output in either case.
 
-'''
 class Game:
 
-    player_types = {'HUMAN' : human.Human, 'PLAYER' : player.Player}
+    player_types = {'HUMAN' : human.Human, 'PLAYER' : player.Player} # (dic) key: String connected to possible players, value: function for player objects initialisation.
 
+    # initialises object attributes according to input values. 
+    # Input: type_player1/2 (string, key to dictionary player_types), name_player1/2 (string, name of player1/2) 
+    # Changes: self.graphcial, (self.graphical_interaction), self.name_player1/2, self.num_disks_player1/2, self.winner, self.game_board.
     def __init__(self, type_player1, name_player1, type_player2, name_player2, graphical):
 
+        # If at least one player is human or graphcial = True then graphical is True and graphcial_interaction is initialised else graphical is False 
         if(graphical or type_player1 == 'HUMAN' or type_player2 == 'HUMAN'):
             self.graphical = True
             self.graphical_interaction = interaction.Interaction()
         else:
             self.graphical = False
 
+        # initialistion of player1/2 according to input variable type_player1/2
         if(type_player1 == 'HUMAN'):
             self.player1 = human.Human(1,self.graphical_interaction)
         else:
@@ -32,16 +35,17 @@ class Game:
         else:
             self.player2 = Game.player_types[type_player2](-1)
         
+        # names of players are used for output
         self.name_player1 = name_player1
         self.name_player2 = name_player2
 
         self.winner = None
-        self.num_disks_player1 = 2
-        self.num_disks_player2 = 2
-        self.game_board = board.Board()
+        self.num_disks_player1 = 2 # (int) number of disks player 1 has after game was played.
+        self.num_disks_player2 = 2 # (int) number of disks player 2 has after game was played.
+        self.game_board = board.Board() # (board object) current board object (state of the game).
 
 
-
+    # calls function run_game_graphical() or run_game_non_graphical() depending on the value of self.graphical.
     def run_game(self):
 
         if(self.graphical):
@@ -49,6 +53,10 @@ class Game:
         else:
             self.run_game_non_graphical()
     
+    # simulates one game between player1/2 with graphical output/interaction. loops over turn player1, turn player2 until
+    # no more empty positions are available, both players passed or a human player clicked quit. For turn from player1/2 
+    # calls player object function make_move(self.game_board). After each turn draws new board on window 
+    # Changes: self.game_board, self.winner, self.num_disks_player1/2, self.window
     def run_game_graphical(self):
 
         p1_made_move = 0
@@ -58,15 +66,30 @@ class Game:
 
         while(self.game_board.empty_positions > 0):
 
+            # draw board on screen, wait for some time such that user can see board
             self.graphical_interaction.draw_board(self.game_board)
+
+            # player1 moves
             p1_quit, p1_made_move, self.game_board = self.player1.make_move(self.game_board)
+
+            # exit game loop if player 2 selected quit
             if p1_quit:               
                 break
+
+            # draw board on screen, wait for some time such that user can see board
             self.graphical_interaction.draw_board(self.game_board)
+
+            # player2 moves
             p2_quit, p2_made_move, self.game_board = self.player2.make_move(self.game_board)
+
+            # exit game loop if player 2 selected quit
             if p2_quit:    
                 break
+
+            # draw board on screen, wait for some time such that user can see board
             self.graphical_interaction.draw_board(self.game_board)
+
+            # if both players passed calculate number of diks for each player, determine winner leave game loop
             if(not p1_made_move and not p2_made_move):
                 if(self.game_board.disks_black > self.game_board.disks_white):
                     self.winner = self.name_player2
@@ -76,7 +99,7 @@ class Game:
                 self.num_disks_player1 = self.game_board.disks_white
                 break
                 
-
+            # if no more empty positions calculate number of diks for each player, determine winner leave game loop
             if(self.game_board.empty_positions == 0):
                 if(self.game_board.disks_black > self.game_board.disks_white):
                     self.winner = self.name_player2
