7172051b4a4cf23a1499718f2b12d61ba4ee6976
Paula <paula.b.hofm@gmail.com>
2021-05-14 14:29:56 +0200

    added comments

--- Othello_Game/board.py
+++ Othello_Game/board.py
@@ -1,23 +1,29 @@
 '''
-
-Class to represent the game board. The positions on the board are saved as a two dimensional list where the first dimension
-represents the row of the board and the second dimension the column. Each position is eihter unoccupied (0), white (1) or black (-1).
-
+Board object is for the representation of the game board. It contains the current state of the game.
 '''
 import othello_game.constants as c
 
 class Board:
 
+    # initializes object attributes, all of them are determined by the rules of the game, calls function create_board().
+    # Changes: self. positions, self.disks_black/white, self.num_positions, self.empty_positions, self_num_rows/cols.
     def __init__(self):
-        self.positions = []
+        # (2d List) containing the actual game board representation. 
+        # Each inner list contains one row of the board. Each element is either 0 (empty), 1 (white) or -1 (black).
+        self.positions = [] 
+
         self.disks_black = 2
         self.disks_white = 2
         self.num_positions = (c.NUM_ROWS * c.NUM_ROWS)
         self.empty_positions = self.num_positions - 4
         self.num_rows = c.NUM_ROWS
         self.num_cols = c.NUM_COLS
+
+        # call func to initialise positions 
         self.create_board()
 
+    # initialises the board positions according to the rules.  
+    # Changes: self.positions
     def create_board(self):
 
         # initialise board with unoccupied positions
@@ -32,6 +38,8 @@ class Board:
         self.positions[4][3] = -1
         self.positions[4][4] = 1
     
+    # calculates current number of disks in each colour and the empty positions. 
+    # Changes: self.disks_black/white, self.empty_positions.
     def update_num_diks(self):
         temp_disks_black = 0
         temp_disks_white = 0
