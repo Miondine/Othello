41dc8d5e156f4019ea26315d925e919262f5a6ee
Paula <paula.b.hofm@gmail.com>
2021-05-17 19:12:18 +0200

    changed one player to roxanne

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import othello_game.game as game
 
-game1 = game.Game('HUMAN','Axel','HUMAN','Paula',True)
+game1 = game.Game('ROXANNE','Roxanne','HUMAN','Paula',True)
 game1.run_game()
 print(f'Winner: {game1.winner}')
 print(f'Number of Disks: {game1.name_player1} : {game1.num_disks_player1}, {game1.name_player2} : {game1.num_disks_player2}')
