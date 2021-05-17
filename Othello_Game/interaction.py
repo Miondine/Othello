f85d36a7fd503ea98bb4d7f1e035f4a216654d3d
Paula <paula.b.hofm@gmail.com>
2021-05-17 18:39:43 +0200

    add start button, comemnts, change next button

--- Othello_Game/interaction.py
+++ Othello_Game/interaction.py
@@ -13,9 +13,11 @@ class Interaction:
 
     clock = pygame.time.Clock() # (pygame object) needed for pygame implementations when user input needs to be checked regularly.
     FPS = 60 # determines how fast the pygame clock works.
-    next_font = pygame.font.SysFont('helvetica',c.NEXT_BUTTON_FONTSIZE)
-    next_img = next_font.render('Next', True, c.BLACK)
-    
+    next_font = pygame.font.SysFont('helvetica',c.NEXT_BUTTON_FONTSIZE) # font object for next button 
+    next_img = next_font.render('Next', True, c.BLACK) # next button text
+    start_font = pygame.font.SysFont('helvetica',c.NEXT_BUTTON_FONTSIZE) # font object for start button
+    start_img = next_font.render('Start', True, c.BLACK) # start button text
+
     # initialises object attributes; setup of output/interaction window; sets caption of window to ‘Othello‘. 
     # Changes: self.window.
     def __init__(self):
@@ -31,10 +33,6 @@ class Interaction:
 
         # draw board background
         pygame.draw.rect(self.window,c.GREY,(c.MARGIN_SIDE,c.MARGIN_TOP,c.BOARD_SIZE,c.BOARD_SIZE))
-        
-        #draw next button
-        pygame.draw.rect(self.window,c.DARK_GREY,(c.NEXT_BUTTON_x,c.NEXT_BUTTON_y,c.NEXT_BUTTON_WIDTH,c.NEXT_BUTTON_HEIGHT))
-        self.window.blit(Interaction.next_img, (c.NEXT_BUTTON_x + int(c.NEXT_BUTTON_WIDTH * 0.05),c.NEXT_BUTTON_y + int(c.NEXT_BUTTON_HEIGHT * 0.2)))
 
         # draw squares on board
         for row in range(c.NUM_ROWS):
@@ -47,6 +45,24 @@ class Interaction:
                 elif(board.positions[row][col] == -1):         
                     pygame.draw.circle(self.window, c.BLACK, (c.MARGIN_SIDE + (col + 0.5) * c.SQUARE_SIZE, c.MARGIN_TOP + (row + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
         pygame.display.update()
+
+    # draws start button on board. 
+    # Changes: self.window
+    def draw_start_button(self):
+
+        pygame.draw.rect(self.window,c.RED,(c.NEXT_BUTTON_x,c.NEXT_BUTTON_y,c.NEXT_BUTTON_WIDTH,c.NEXT_BUTTON_HEIGHT))
+        self.window.blit(Interaction.start_img, (c.NEXT_BUTTON_x + int(c.NEXT_BUTTON_WIDTH * 0.05),c.NEXT_BUTTON_y + int(c.NEXT_BUTTON_HEIGHT * 0.2)))
+        pygame.display.update()
+
+    # draw_next_button(self): draws next button on board. 
+    # Changes: self.window
+    def draw_next_button(self):
+
+        pygame.draw.rect(self.window,c.DARK_GREY,(c.NEXT_BUTTON_x,c.NEXT_BUTTON_y,c.NEXT_BUTTON_WIDTH,c.NEXT_BUTTON_HEIGHT))
+        self.window.blit(Interaction.next_img, (c.NEXT_BUTTON_x + int(c.NEXT_BUTTON_WIDTH * 0.05),c.NEXT_BUTTON_y + int(c.NEXT_BUTTON_HEIGHT * 0.2)))
+        pygame.display.update()
+
+
 
     # draws all possible positions on the window (only the positions, the board needs to be drawn on the window first). Colour of positions depends
     # on for which player moves are drawn. 
@@ -70,7 +86,7 @@ class Interaction:
         pygame.draw.circle(self.window, c.DARK_RED, (c.MARGIN_SIDE + (position[1] + 0.5) * c.SQUARE_SIZE, c.MARGIN_TOP + (position[0] + 0.5) * c.SQUARE_SIZE ), c.RADIUS_DISK)
         pygame.display.update()
 
-    # waits for user mouse click, if user clicks exit returns exit code, if user clicks somewhere else function calculates from pixel position 
+    # waits for user mouse click, if user clicks exit returns exit code, if user clicks somewhere else function calculates from pixel position, 
     # position in [row,col] format. 
     # Output: selected_position (list in format [row,col])
     def get_mouse_position(self):
@@ -86,7 +102,9 @@ class Interaction:
                     col = int((x - c.MARGIN_SIDE) / c.SQUARE_SIZE)
                     return [row,col]
 
-
+    # waits for user mouse click, if user clicks exit returns quit. If user clicks somewhere else function calculates if user clicked on next button.
+    # If he did, returns not quit. 
+    # Output: quit_value (True is user wants to exit, False if clicked on next/start)
     def get_next_click(self):
 
         quit_val = None
@@ -102,3 +120,7 @@ class Interaction:
                         quit_val = False
         return quit_val
 
+    def display_string(self,string):
+
+        font = pygame.draw.rect(self.window,c.DARK_GREY,(c.NEXT_BUTTON_x,c.NEXT_BUTTON_y,c.NEXT_BUTTON_WIDTH,c.NEXT_BUTTON_HEIGHT))
+        self.window.blit(Interaction.next_img, (c.NEXT_BUTTON_x + int(c.NEXT_BUTTON_WIDTH * 0.05),c.NEXT_BUTTON_y + int(c.NEXT_BUTTON_HEIGHT * 0.2)))
