7d108b5dfe61b7e02462f3d17f44e8e46c7bd17f
Paula <paula.b.hofm@gmail.com>
2021-07-07 18:30:42 +0200

    removed spaces in name in text output

--- main3.py
+++ main3.py
@@ -1,5 +1,4 @@
 import othello_game.game as game
-import othello_player.playertimer as playertimer
 
 num_games = 100
 type_player1 = 'ALPHA_BETA'
@@ -7,38 +6,38 @@ type_player2 = 'ROXANNE'
 name_player1 = 'AlphaBeta'
 name_player2 = 'Roxanne'
 
-f = open(f'{name_player1}4_{name_player2}_{num_games}.txt', 'w')
+f = open(f'{name_player1}_{name_player2}_{num_games}.txt', 'w')
 f.write(f'{num_games} games {name_player1} against {name_player2} for {name_player1}: white and {name_player2}: black.\n')
 f.write('"In first two columns winner gets 1 looser 0"\n')
 f.write('\n')
-f.write(f'{name_player1} Winning, {name_player2} Winning, Disks {name_player1}, Disks {name_player2}\n')
+f.write(f'{name_player1}_Winning,{name_player2}_Winning,Disks_{name_player1},Disks_{name_player2}\n')
 
 for x in range(num_games):
     game1 = game.Game(type_player1,name_player1,type_player2,name_player2,False)
     game1.run_game()
     if(game1.winner == name_player1):
-        f.write(f'1,0, {game1.num_disks_player1}, {game1.num_disks_player2} \n')
+        f.write(f'1,0,{game1.num_disks_player1},{game1.num_disks_player2}\n')
     elif(game1.winner == name_player2):
-        f.write(f'0, 1, {game1.num_disks_player1}, {game1.num_disks_player2} \n')
+        f.write(f'0,1,{game1.num_disks_player1},{game1.num_disks_player2}\n')
     else:
-        f.write(f'0, 0, {game1.num_disks_player1}, {game1.num_disks_player2} \n')
+        f.write(f'0,0,{game1.num_disks_player1},{game1.num_disks_player2}\n')
 
 f.close()
 
-f = open(f'{name_player2}_{name_player1}4_{num_games}.txt', 'w')
+f = open(f'{name_player2}_{name_player1}_{num_games}.txt', 'w')
 f.write(f'{num_games} games {name_player2} against {name_player1} for {name_player2}: white and {name_player1}: black.\n')
 f.write('"In first two columns winner gets 1 looser 0"\n')
 f.write('\n')
-f.write(f'{name_player1} Winning, {name_player2} Winning, Disks {name_player1}, Disks {name_player2}\n')
+f.write(f'{name_player1}_Winning,{name_player2}_Winning,Disks_{name_player1},Disks_{name_player2}\n')
 
 for x in range(num_games):
     game1 = game.Game(type_player2,name_player2,type_player1,name_player1,False)
     game1.run_game()
     if(game1.winner == name_player1):
-        f.write(f'1, 0, {game1.num_disks_player2}, {game1.num_disks_player1} \n')
+        f.write(f'1,0,{game1.num_disks_player2},{game1.num_disks_player1}\n')
     elif(game1.winner == name_player2):
-        f.write(f'0, 1, {game1.num_disks_player2}, {game1.num_disks_player1} \n')
+        f.write(f'0,1,{game1.num_disks_player2},{game1.num_disks_player1}\n')
     else:
-        f.write(f'0, 0, {game1.num_disks_player2}, {game1.num_disks_player1} \n')
+        f.write(f'0,0,{game1.num_disks_player2},{game1.num_disks_player1}\n')
 
 f.close()
