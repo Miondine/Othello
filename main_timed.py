f15f41606a90d97ed2b2aea721a9a2716b7c6635
Paula <paula.b.hofm@gmail.com>
2021-07-08 13:30:18 +0200

    changed timer output all to one file

--- main_timed.py
+++ main_timed.py
@@ -14,8 +14,8 @@ f.write('"In first two columns winner gets 1 looser 0"\n')
 f.write('\n')
 f.write(f'{name_player1}_Winning,{name_player2}_Winning,Disks_{name_player1},Disks_{name_player2}\n')
 
-player_timer1 = playertimer.PlayerTimer(name_player1,num_games,1)
-player_timer2 = playertimer.PlayerTimer(name_player2,num_games,-1)
+player_timer1 = playertimer.PlayerTimer(name_player1,2 * num_games)
+player_timer2 = playertimer.PlayerTimer(name_player2,2 * num_games)
 
 for x in range(num_games):
     game1 = game.Game(type_player1,name_player1,type_player2,name_player2,False)
@@ -31,9 +31,6 @@ for x in range(num_games):
     else:
         f.write(f'0, 0, {game1.num_disks_player1}, {game1.num_disks_player2} \n')
 
-player_timer1.close_file()
-player_timer2.close_file()
-
 f.close()
 
 f = open(f'{name_player2}_{name_player1}_{num_games}.txt', 'w')
@@ -42,14 +39,12 @@ f.write('"In first two columns winner gets 1 looser 0"\n')
 f.write('\n')
 f.write(f'{name_player1}_Winning,{name_player2}_Winning,Disks_{name_player1},Disks_{name_player2}\n')
 
-player_timer1 = playertimer.PlayerTimer(name_player2,num_games,1)
-player_timer2 = playertimer.PlayerTimer(name_player1,num_games,-1)
 
 for x in range(num_games):
     game1 = game.Game(type_player2,name_player2,type_player1,name_player1,False)
     player_timer1.start_game()
     player_timer2.start_game()
-    game1.run_game_timed(player_timer1,player_timer2)
+    game1.run_game_timed(player_timer2,player_timer1)
     player_timer1.stop_game()
     player_timer2.stop_game()
     if(game1.winner == name_player1):
