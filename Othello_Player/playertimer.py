8426f8850f7d7d3c35b0e138db00168864ec9360
Paula <paula.b.hofm@gmail.com>
2021-07-10 20:21:02 +0200

    debugged

--- Othello_Player/playertimer.py
+++ Othello_Player/playertimer.py
@@ -2,10 +2,11 @@ import time
 
 class PlayerTimer:
     
-    def __init__(self, player_name, num_games):
+    def __init__(self, player_name, opponent_name,num_games):
         
         self.num_games = num_games
         self.player_name = player_name
+        self.opponent_name = opponent_name
         self.start_time = None
         self.game_counter = 1
         self.move_counter = 0
@@ -13,17 +14,17 @@ class PlayerTimer:
         self.max_moves = 50
         self.move_time_summed = [0.0 for x in range(self.max_moves)]
         self.move_executed = [0 for x in range(self.max_moves)]
-        self.game_time_summed = [0.0 for x in range(self.max_moves)]
+        self.game_time_summed = [0.0 for x in range(self.num_games)]
         self.setup_file()
 
     def setup_file(self):
 
         
-        self.file_string = f'movetime_{self.player_name}_{self.num_games}.txt'
+        self.file_string = f'movetime_{self.player_name}_{self.num_games}_{self.opponent_name}.txt'
 
         self.file = open(self.file_string,'w')
 
-        self.file.write(f'Contains move time for player {self.player_name}\n')
+        self.file.write(f'Contains move time for player {self.player_name}; opponent is {self.opponent_name}\n')
         self.file.write(f'Colums refer to move number, rows to game number\n')
 
         self.file.write('game,')
@@ -63,6 +64,10 @@ class PlayerTimer:
         for x in self.move_time_summed:
             self.file.write(f'{x}, ')
         self.file.write(f'{sum(self.move_time_summed)}\n')
+        self.file.write('number move x executed,')
+        for x in self.move_executed:
+            self.file.write(f'{x},')
+        self.file.write(f'{self.num_games}\n')
         self.file.write('average time per move x,')
         for x, y in zip(self.move_time_summed,self.move_executed):
             if(y == 0):
