ef449bb56275bec4e4f742640b6b3432bf66023e
Paula <paula.b.hofm@gmail.com>
2021-08-07 20:46:38 +0200

    changed names of variables

--- Othello_Game/game.py
+++ Othello_Game/game.py
@@ -30,53 +30,53 @@ class Game:
     depth_players = ['NEGAMAX','ALPHA_BETA','STATIC_BOARD','DYNAMIC_BOARD','MCTS_MAX_ITER','MCTS_REM_MAX_ITER','EDAX']
 
     # initialises object attributes according to input values. 
-    # Input: type_black/white_player (string, key to dictionary player_types), name_black/white_player (string, name of player1/2) 
-    # Changes: self.graphcial, (self.graphical_interaction), self.name_black/white_player, self.type_black/white_player, self.num_discs_player1/2, self.winner, self.game_board.
-    def __init__(self, type_black_player, name_black_player, type_white_player, name_white_player, graphical = None, depth_player1 = None, depth_player2 = None):
+    # Input: type_black/white (string, key to dictionary player_types), name_black/white (string, name of black/2) 
+    # Changes: self.graphcial, (self.graphical_interaction), self.name_black/white, self.type_black/white, self.num_discs_black/2, self.winner, self.game_board.
+    def __init__(self, type_black, name_black, type_white, name_white, graphical = None, depth_black = None, depth_white = None):
 
         # If at least one player is human or graphcial = True then graphical is True and graphcial_interaction is initialised else graphical is False 
-        if(graphical or type_black_player == 'HUMAN' or type_white_player == 'HUMAN'):
+        if(graphical or type_black == 'HUMAN' or type_white == 'HUMAN'):
             self.graphical = True
             self.graphical_interaction = interaction.Interaction()
             # strings for gui information
-            self.turn_player1 = f"{name_black_player}'s turn!"
-            self.turn_player2 = f"{name_white_player}'s turn!"
-            self.moved_player1 = f"{name_black_player} moved"
-            self.moved_player2 = f"{name_white_player} moved"
-            self.passed_player1 = f"{name_black_player} passed"
-            self.passed_player2 = f"{name_white_player} passsed"
+            self.turn_black = f"{name_black}'s turn!"
+            self.turn_white = f"{name_white}'s turn!"
+            self.moved_black = f"{name_black} moved"
+            self.moved_white = f"{name_white} moved"
+            self.passed_black = f"{name_black} passed"
+            self.passed_white = f"{name_white} passsed"
         else:
             self.graphical = False
             self.graphical_interaction = None
 
-        # initialistion of player1/2 according to input variable type_black/white_player
+        # initialistion of black/2 according to input variable type_black/white
         
-            if(type_black_player in Game.depth_players):
-                self.player1 = Game.player_types[type_black_player](colour = 1,graphical = self.graphical, graphical_interface = self.graphical_interaction, depth = depth_player1)
+            if(type_black in Game.depth_players):
+                self.black = Game.player_types[type_black](colour = 1,graphical = self.graphical, graphical_interface = self.graphical_interaction, depth = depth_black)
             else:
-                self.player1 = Game.player_types[type_black_player](colour = 1,graphical = self.graphical, graphical_interface = self.graphical_interaction)
-            if(type_white_player in Game.depth_players):
-                self.player2 = Game.player_types[type_white_player](colour = -1,graphical = self.graphical, graphical_interface = self.graphical_interaction, depth = depth_player2)
+                self.black = Game.player_types[type_black](colour = 1,graphical = self.graphical, graphical_interface = self.graphical_interaction)
+            if(type_white in Game.depth_players):
+                self.white = Game.player_types[type_white](colour = -1,graphical = self.graphical, graphical_interface = self.graphical_interaction, depth = depth_white)
             else:
-                self.player2 = Game.player_types[type_white_player](colour = -1,graphical = self.graphical, graphical_interface = self.graphical_interaction)
+                self.white = Game.player_types[type_white](colour = -1,graphical = self.graphical, graphical_interface = self.graphical_interaction)
+                
             
         
-        self.type_player1 = type_black_player
-        self.type_player2 = type_white_player
+        self.type_black = type_black
+        self.type_white = type_white
 
-        # names of players are used for output
-        self.name_player1 = name_black_player
-        self.name_player2 = name_white_player
+        self.name_black = name_black
+        self.name_white = name_white
 
         self.winner = None
-        self.num_discs_player1 = 2 # (int) number of discs player 1 has after game was played.
-        self.num_discs_player2 = 2 # (int) number of discs player 2 has after game was played.
+        self.num_discs_black = 2 # (int) number of discs black player has after game was played.
+        self.num_discs_white = 2 # (int) number of discs white player has after game was played.
         self.game_board = board.Board() # (board object) current board object (state of the game).
 
     def close_game(self):
 
-        self.player1.close_player()
-        self.player2.close_player()
+        self.black.close_player()
+        self.white.close_player()
 
     # calls function run_game_graphical() or run_game_non_graphical() depending on the value of self.graphical.
     def run_game(self):
@@ -86,11 +86,11 @@ class Game:
         else:
             self.run_game_non_graphical()
     
-    # simulates one game between player1/2 with graphical output/interaction. loops over turn player1, turn player2 until
-    # no more empty positions are available, both players passed or a human player clicked quit. For turn from player1/2 
+    # simulates one game between black/2 with graphical output/interaction. loops over turn black, turn white until
+    # no more empty positions are available, both players passed or a human player clicked quit. For turn from black/2 
     # calls player object function make_move_graphical(self.game_board). After each turn draws new board on window. If 
     # player is not human, asks user to click next.
-    # Changes: self.game_board, self.winner, self.num_discs_player1/2, self.window
+    # Changes: self.game_board, self.winner, self.num_discs_black/2, self.window
     def run_game_graphical(self):
 
         # draw board on screen, wait for start click
@@ -102,25 +102,25 @@ class Game:
             exit()
 
         
-        p1_made_move = True
-        p2_made_move = True
+        black_made_move = True
+        white_made_move = True
 
         while True:
 
             self.graphical_interaction.draw_board(self.game_board)
-            self.graphical_interaction.display_string(self.turn_player1)
+            self.graphical_interaction.display_string(self.turn_black)
             pygame.time.wait(1000)
 
-            # player1 moves
-            p1_quit, p1_made_move, position, self.game_board = self.player1.make_move_graphical(self.game_board)
+            # black moves
+            black_quit, black_made_move, position, self.game_board = self.black.make_move_graphical(self.game_board)
 
-            # exit game loop if player 2 selected quit
-            if p1_quit:               
+            # exit game loop if black player selected quit
+            if black_quit:               
                 self.close_game()
                 exit()
 
             # if both players passed calculate number of diks for each player, determine winner leave game loop
-            if(not p1_made_move and not p2_made_move):
+            if(not black_made_move and not white_made_move):
                 self.game_finish_graphical()
                 break
                 
@@ -130,13 +130,13 @@ class Game:
                 break
 
             # draw board on screen and if player moved selected position.
-            if p1_made_move:
+            if black_made_move:
                 self.graphical_interaction.draw_board(self.game_board)
-                self.graphical_interaction.display_string(self.moved_player1)
+                self.graphical_interaction.display_string(self.moved_black)
                 self.graphical_interaction.draw_selected_position(position)
             else:
                 self.graphical_interaction.draw_board(self.game_board)
-                self.graphical_interaction.display_string(self.passed_player1)
+                self.graphical_interaction.display_string(self.passed_black)
 
             # wait until user clicks next
             quit_val = self.graphical_interaction.get_next_click()
@@ -145,19 +145,19 @@ class Game:
                 exit()
 
             self.graphical_interaction.draw_board(self.game_board)
-            self.graphical_interaction.display_string(self.turn_player2)
+            self.graphical_interaction.display_string(self.turn_white)
             pygame.time.wait(1000)
 
-            # player2 moves
-            p2_quit, p2_made_move, position, self.game_board = self.player2.make_move_graphical(self.game_board)
+            # white moves
+            white_quit, white_made_move, position, self.game_board = self.white.make_move_graphical(self.game_board)
 
-            # exit game loop if player 2 selected quit
-            if p2_quit:    
+            # exit game loop if black player selected quit
+            if white_quit:    
                 self.close_game()
                 exit()
 
             # if both players passed calculate number of diks for each player, determine winner leave game loop
-            if(not p1_made_move and not p2_made_move):
+            if(not black_made_move and not white_made_move):
                 self.game_finish_graphical()
                 break
                 
@@ -167,13 +167,13 @@ class Game:
                 break
 
             # draw board on screen and if player moved selected position 
-            if p2_made_move:
+            if white_made_move:
                 self.graphical_interaction.draw_board(self.game_board)
-                self.graphical_interaction.display_string(self.moved_player2)
+                self.graphical_interaction.display_string(self.moved_white)
                 self.graphical_interaction.draw_selected_position(position)
             else:
                 self.graphical_interaction.draw_board(self.game_board)
-                self.graphical_interaction.display_string(self.passed_player2)
+                self.graphical_interaction.display_string(self.passed_white)
 
             # wait until user clicks next
             quit_val = self.graphical_interaction.get_next_click()
@@ -186,18 +186,18 @@ class Game:
     def game_finish_graphical(self):
 
         if(self.game_board.discs_black > self.game_board.discs_white):
-            self.winner = self.name_player1
+            self.winner = self.name_black
             # assign all empty positions to the winner
             self.game_board.discs_black += self.game_board.empty_positions
         elif(self.game_board.discs_black < self.game_board.discs_white):
-            self.winner = self.name_player2
+            self.winner = self.name_white
             # assign all empty positions to the winner
             self.game_board.discs_white += self.game_board.empty_positions
-        self.num_discs_player2 = self.game_board.discs_white
-        self.num_discs_player1 = self.game_board.discs_black
+        self.num_discs_white = self.game_board.discs_white
+        self.num_discs_black = self.game_board.discs_black
 
         self.graphical_interaction.draw_board(self.game_board)
-        self.graphical_interaction.display_string(f"The winner is {self.winner}. {self.name_player1} has {self.num_discs_player1} discs and {self.name_player2} has {self.num_discs_player2} discs")
+        self.graphical_interaction.display_string(f"The winner is {self.winner}. {self.name_black} has {self.num_discs_black} discs and {self.name_white} has {self.num_discs_white} discs")
         # wait until player clicks next button or quit to exit game
         self.graphical_interaction.draw_next_button()
         quit_val = self.graphical_interaction.get_next_click()
@@ -205,20 +205,20 @@ class Game:
             self.close_game()
             exit()
 
-    # simulates one game between player1/2. Loops over turn player1, turn player2 until no more empty positions are available or both
-    # players passed. For turn from player1/2 calls player object function make_move(self.game_board). 
-    # Changes: self.game_board, self.winner, self.num_discs_player1/2.
+    # simulates one game. Loops over turn black, turn white until no more empty positions are available or both
+    # players passed. For turn from black/white calls player object function make_move(self.game_board). 
+    # Changes: self.game_board, self.winner, self.num_discs_black/white.
     def run_game_non_graphical(self):
 
-        p1_made_move = True
-        p2_made_move = True
+        black_made_move = True
+        white_made_move = True
         while True:
 
-            # player1 moves
-            p1_made_move, self.game_board = self.player1.make_move(self.game_board)
+            # black moves
+            black_made_move, self.game_board = self.black.make_move(self.game_board)
 
             # if both players passed calculate number of diks for each player, determine winner leave game loop
-            if(not p1_made_move and not p2_made_move):
+            if(not black_made_move and not white_made_move):
                 self.game_finish()
                 break
                 
@@ -227,11 +227,11 @@ class Game:
                 self.game_finish()
                 break
 
-            # player2 moves
-            p2_made_move, self.game_board = self.player2.make_move(self.game_board)
+            # white moves
+            white_made_move, self.game_board = self.white.make_move(self.game_board)
 
             # if both players passed calculate number of diks for each player, determine winner leave game loop
-            if(not p1_made_move and not p2_made_move):
+            if(not black_made_move and not white_made_move):
                 self.game_finish()
                 break
                 
@@ -243,32 +243,30 @@ class Game:
     def game_finish(self):
 
         if(self.game_board.discs_black > self.game_board.discs_white):
-            self.winner = self.name_player1
+            self.winner = self.name_black
             # assign all empty positions to the winner
             self.game_board.discs_black += self.game_board.empty_positions
         elif(self.game_board.discs_black < self.game_board.discs_white):
-            self.winner = self.name_player2
+            self.winner = self.name_white
             # assign all empty positions to the winner
             self.game_board.discs_white += self.game_board.empty_positions
-        self.num_discs_player2 = self.game_board.discs_white
-        self.num_discs_player1 = self.game_board.discs_black
+        self.num_discs_white = self.game_board.discs_white
+        self.num_discs_black = self.game_board.discs_black
 
-    def run_game_timed(self,timer_player1,timer_player2):
+    def run_game_timed(self,timer_black,timer_white):
 
-        p1_made_move = True
-        p2_made_move = True
-
-        
+        black_made_move = True
+        white_made_move = True      
      
         while True:
 
-            # player1 moves
-            timer_player1.start_move()
-            p1_made_move, self.game_board = self.player1.make_move(self.game_board)
-            timer_player1.stop_move()
+            # black moves
+            timer_black.start_move()
+            black_made_move, self.game_board = self.black.make_move(self.game_board)
+            timer_black.stop_move()
             
             # if both players passed calculate number of diks for each player, determine winner leave game loop
-            if(not p1_made_move and not p2_made_move):
+            if(not black_made_move and not white_made_move):
                 self.game_finish()
                 break
 
@@ -277,13 +275,13 @@ class Game:
                 self.game_finish()
                 break
 
-            # player2 moves
-            timer_player2.start_move()
-            p2_made_move, self.game_board = self.player2.make_move(self.game_board)
-            timer_player2.stop_move()
+            # white moves
+            timer_white.start_move()
+            white_made_move, self.game_board = self.white.make_move(self.game_board)
+            timer_white.stop_move()
 
             # if both players passed calculate number of diks for each player, determine winner leave game loop
-            if(not p1_made_move and not p2_made_move):
+            if(not black_made_move and not white_made_move):
                 self.game_finish()
                 break
        
